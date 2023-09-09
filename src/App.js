f3374f17d66b852d9e72ae60e6c86af3b3546473
padma Talla <venkatapadmat91@gmail.com>
2023-09-09 05:30:13 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* https://alertnotice345.ccbp.tech */
+
+import AlertNotifications from './components/AlertNotifications'
+
+import './App.css'
+
+const App = () => <AlertNotifications />
+
+export default App
